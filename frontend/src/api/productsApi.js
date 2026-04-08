67919fd63c5693c5c1a5abf73828e237df24842e
Sahar-Lamine <sahar@eminence.ch>
2026-04-08 09:29:10 +0100

    add variable

--- frontend/src/api/productsApi.js
+++ frontend/src/api/productsApi.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE_URL;
+const API_BASE = "https://backend-dusky-delta-10.vercel.app/api";
 
 export async function fetchProducts(search = "") {
   const url = search
